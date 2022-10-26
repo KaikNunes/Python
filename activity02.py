69c1afe02f58d7f84f5e4a38559a0b2aba192382
Kaik Nunes Oliveira <113046021+KaikNunes@users.noreply.github.com>
2022-10-26 14:11:27 -0300

    Activity 02

---
+++ activity02.py
@@ -0,0 +1,7 @@
+# adicionando valor as variáveis
+string = input("Escreva algo: ")
+inteiro = input("Escreva seu número inteiro: ")
+
+# escrevendo na tela
+print("Frase escrita: ", str(string))
+print("Valor inteiro: ", int(inteiro))
