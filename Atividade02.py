5f3f3555fec0524ad7ee2c893fcffcba6026a26e
Kaik Nunes Oliveira <113046021+KaikNunes@users.noreply.github.com>
2022-10-26 14:11:42 -0300

    Delete Atividade02.py

--- Atividade02.py
+++
@@ -1,7 +0,0 @@
-# adicionando valor as variáveis
-string = input("Escreva algo: ")
-inteiro = input("Escreva seu número inteiro: ")
-
-# escrevendo na tela
-print("Valor inteiro: ", int(inteiro))
-print("Frase escrita: ", str(string))
