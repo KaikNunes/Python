45c168060977ce9cc34037914b4357a3d2c9c22e
Kaik Nunes Oliveira <113046021+KaikNunes@users.noreply.github.com>
2022-10-26 21:29:42 -0300

    Activity 05

---
+++ activity05.py
@@ -0,0 +1,12 @@
+#recebimento do número
+n = float(input("Escreva seu número: "))
+
+#condicional se (if, elif, else)
+if n < 10:
+    print("Seu número é menor que 10")
+elif n == 10:
+    print("Seu número é igual a 10")
+elif n > 10:
+    print("Seu número é maior que 10")
+else:
+    print("Verifique os dados enviados")
