abfa49d86dd4cb08af4c95bae0e1db91bb877e90
Kaik Nunes Oliveira <113046021+KaikNunes@users.noreply.github.com>
2022-10-27 21:54:25 -0300

    Activity07

---
+++ activity07.py
@@ -0,0 +1,8 @@
+# Recebendo informações do usuário
+numeroTabuada = int(input("Qual tabuada você quer? "))
+numeroInicial = int(input("Digite qual primeiro múltiplo: "))
+numeroFinal = int(input("Digite qual último multiplo: "))
+
+# Repetição para
+for i in range (numeroInicial, numeroFinal+1):
+    print((numeroTabuada), "*", i, "=", i * numeroTabuada)
