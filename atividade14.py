214df035a8745ef9e8b46ebdba0b553a27f7ad88
Kaik Nunes Oliveira <113046021+KaikNunes@users.noreply.github.com>
2022-11-02 15:03:26 -0300

    Delete atividade14.py

--- atividade14.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input("Digite o multiplicador: "))
-n2 = int(input("Digite o multiplicando: "))
-
-def multiplicacao():
-    resultado = n1 * n2
-    print("O resultado é: ", resultado)
-
-multiplicacao()
